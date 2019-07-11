8ed87daeafd51e83e98021c61a71e52228decff9
Alkhanm <Joas7777777@hotmail.com>
2019-07-11 19:43:34 -0300

    Remove department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,7 +1,9 @@
 package gui.util;
 
+import java.util.Optional;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 public class Alerts {
 	public static void showAlert(String title, String header, String content, AlertType type ) {
@@ -12,4 +14,11 @@ public class Alerts {
 		aler.show();
 
 	}
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
+		}
 }
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -21,5 +21,8 @@ public class DepartmentService {
 		}
 	}
 	
+	public void remove(Department obj) {
+		dao.deleteByID(obj.getId());
+	}
 
 }
